79fcde87e7d36d0ee487298de58daa7641ee06bb
cohadruid <dariocrc11@gmail.com>
2018-12-06 18:45:17 +0100

    initial commit

---
+++ RUAP_LV4/UnitTest1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class UntitledTestCase2
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "https://www.katalon.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheUntitledTestCase2Test()
+        {
+            driver.Navigate().GoToUrl("http://demowebshop.tricentis.com/");
+            driver.FindElement(By.Id("small-searchterms")).Click();
+            driver.FindElement(By.Id("small-searchterms")).Clear();
+            driver.FindElement(By.Id("small-searchterms")).SendKeys("hard drive");
+            driver.FindElement(By.Id("small-searchterms")).SendKeys(Keys.Enter);
+            driver.FindElement(By.Id("small-searchterms")).Click();
+            driver.FindElement(By.Id("small-searchterms")).Clear();
+            driver.FindElement(By.Id("small-searchterms")).SendKeys("kilogram interneta");
+            driver.FindElement(By.Id("small-searchterms")).SendKeys(Keys.Enter);
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
